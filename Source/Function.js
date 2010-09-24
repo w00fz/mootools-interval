102890764543239750fc964a8eee5ab6c15b6716
Djamil Legato <djamil@djamil.it>
2010-09-24 10:38:15 +0200

    Forgot to add Function.js in the Source.

---
+++ Source/Function.js
@@ -0,0 +1,20 @@
+/*
+---
+name: Function.Interval
+description: A setTimeout wrapper that can pause and resume, with multi functions support.
+license: MIT-style license.
+authors: Djamil Legato (@w00fz)
+requires: [Interval/*]
+provides: Function.Interval
+...
+*/
+
+Function.implement({
+	delay: function(delay, bind, args){
+		return new Interval(this, {delay: delay, repeat: false}).start(args, delay, bind);
+	},
+
+	periodical: function(periodical, bind, args){
+		return new Interval(this, {delay: delay, repeat: true}).start(args, delay, bind);
+	}
+});
